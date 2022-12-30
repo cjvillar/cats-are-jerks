459c9bcbfce2b3b30f6c96b15295b611be6285cd
Chris Villarreal <cjvillar@users.noreply.github.com>
2022-12-30 12:06:31 -0800

    initial project commit

---
+++ CatsAreJerks/Assets/Objects.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour
+{
+    public GameObject Object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -5) { Destroy(Object); }
+    }
+}
